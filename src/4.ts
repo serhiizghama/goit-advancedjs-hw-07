285abe528ab7657e75708c78c484188578f69546
serhii.zghama <serhii.zghama@edetek.com>
2024-11-04 15:16:36 +0700

    Implement HM

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,41 @@
+class Key {
+    private signature: number = Math.random();
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) { }
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    public door: boolean = false;
+    private tenants: Person[] = [];
+
+    constructor(public key: Key) { }
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+        }
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (this.key.getSignature() === key.getSignature()) {
+            this.door = true
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -7,5 +45,4 @@ house.openDoor(person.getKey());
 
 house.comeIn(person);
 
-
-export {};
+export { };
